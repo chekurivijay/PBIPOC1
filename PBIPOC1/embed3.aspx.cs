e8650686f61efb07ec55ad35d22bcb0fc2e4b574
Vijay Chekuri <chekurivijay@ipriya.onmicrosoft.com>
2020-02-24 18:49:27 +0530

    made few changes

---
+++ PBIPOC1/embed3.aspx.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PBIPOC1
+{
+    public class EmbedConfig
+    {
+        public string Id { get; set; }
+
+        public string EmbedUrl { get; set; }
+
+        public EmbedToken EmbedToken { get; set; }
+
+        public int MinutesToExpiration
+        {
+            get
+            {
+                var minutesToExpiration = EmbedToken.Expiration - DateTime.UtcNow;
+                return (int)minutesToExpiration.TotalMinutes;
+            }
+        }
+
+        public bool? IsEffectiveIdentityRolesRequired { get; set; }
+
+        public bool? IsEffectiveIdentityRequired { get; set; }
+
+        public bool EnableRLS { get; set; }
+
+        public string Username { get; set; }
+
+        public string Roles { get; set; }
+
+        public string ErrorMessage { get; internal set; }
+    } //embedconfig class
+
+    public class TileEmbedConfig : EmbedConfig
+    {
+        public string dashboardId { get; set; }
+    }
+    public partial class embed3 : System.Web.UI.Page
+    {
+        private static readonly string AuthorityUrl = "https://login.microsoftonline.com/common/";
+        private static readonly string ResourceUrl = "https://analysis.windows.net/powerbi/api";
+        private static readonly string ApplicationId = "e247842b-b09d-4ff4-8219-c68ebc1efb62";
+        private static readonly string ApiUrl = "https://api.powerbi.com";
+        private static readonly string WorkspaceId = "b3c586fb-b718-4015-8fc9-abdfeabee776";
+        private static readonly string ReportId = "40b87d8e-1305-412b-bb21-e7358adfe3b6";
+
+        private static readonly string AuthenticationType = "MasterUser";
+        //private static readonly NameValueCollection sectionConfig = ConfigurationManager.GetSection(AuthenticationType) as NameValueCollection;
+        private static readonly string ApplicationSecret = "";
+        private static readonly string Tenant = "";
+        private static readonly string Username = "chekurivijay@ipriya.onmicrosoft.com";
+        private static readonly string Password = "EnjoyWork99!";
+
+        private EmbedConfig m_embedConfig;
+        private TileEmbedConfig m_tileEmbedConfig;
+        private TokenCredentials m_tokenCredentials;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public async Task<bool> EmbedReport(string username, string roles)
+        {
+
+            // Get token credentials for user
+            var getCredentialsResult = await GetTokenCredentials();
+            if (!getCredentialsResult)
+            {
+                // The error message set in GetTokenCredentials
+                return false;
+            }
+
+            try
+            {
+                // Create a Power BI Client object. It will be used to call Power BI APIs.
+                using (var client = new PowerBIClient(new Uri(ApiUrl), m_tokenCredentials))
+                {
+                    // Get a list of reports.
+                    var reports = await client.Reports.GetReportsInGroupAsync(Guid.Parse(WorkspaceId));
+
+                    // No reports retrieved for the given workspace.
+                    if (reports.Value.Count() == 0)
+                    {
+                        m_embedConfig.ErrorMessage = "No reports were found in the workspace";
+                        return false;
+                    }
+
+                    Report report;
+                    report = reports.Value.FirstOrDefault();
+                    //if (string.IsNullOrWhiteSpace(ReportId))
+                    //{
+                    //    // Get the first report in the workspace.
+                    //    report = reports.Value.FirstOrDefault();
+                    //}
+                    //else
+                    //{
+                    //    report = reports.Value.FirstOrDefault(r => r.Id.Equals(ReportId, StringComparison.InvariantCultureIgnoreCase));
+                    //}
+
+                    if (report == null)
+                    {
+                        m_embedConfig.ErrorMessage = "No report with the given ID was found in the workspace. Make sure ReportId is valid.";
+                        return false;
+                    }
+
+                    //var datasets = await client.Datasets.GetDatasetByIdInGroupAsync(WorkspaceId, report.DatasetId);
+                    var datasets = await client.Datasets.GetDatasetsInGroupAsync(Guid.Parse(WorkspaceId));
+                    m_embedConfig.IsEffectiveIdentityRequired = datasets.IsEffectiveIdentityRequired;
+                    m_embedConfig.IsEffectiveIdentityRolesRequired = datasets.IsEffectiveIdentityRolesRequired;
+                    GenerateTokenRequest generateTokenRequestParameters;
+                    // This is how you create embed token with effective identities
+                    if (!string.IsNullOrWhiteSpace(username))
+                    {
+                        var rls = new EffectiveIdentity(username, new List<string> { report.DatasetId });
+                        if (!string.IsNullOrWhiteSpace(roles))
+                        {
+                            var rolesList = new List<string>();
+                            rolesList.AddRange(roles.Split(','));
+                            rls.Roles = rolesList;
+                        }
+                        // Generate Embed Token with effective identities.
+                        generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view", identities: new List<EffectiveIdentity> { rls });
+                    }
+                    else
+                    {
+                        // Generate Embed Token for reports without effective identities.
+                        generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view");
+                    }
+
+                    var tokenResponse = await client.Reports.GenerateTokenInGroupAsync(WorkspaceId, report.Id, generateTokenRequestParameters);
+
+                    if (tokenResponse == null)
+                    {
+                        m_embedConfig.ErrorMessage = "Failed to generate embed token.";
+                        return false;
+                    }
+
+                    // Generate Embed Configuration.
+                    m_embedConfig.EmbedToken = tokenResponse;
+                    m_embedConfig.EmbedUrl = report.EmbedUrl;
+                    m_embedConfig.Id = report.Id;
+                }
+            }
+            catch (HttpOperationException exc)
+            {
+                m_embedConfig.ErrorMessage = string.Format("Status: {0} ({1})\r\nResponse: {2}\r\nRequestId: {3}", exc.Response.StatusCode, (int)exc.Response.StatusCode, exc.Response.Content, exc.Response.Headers["RequestId"].FirstOrDefault());
+                return false;
+            }
+
+            return true;
+        } //EmbedReport
+        private async Task<bool> GetTokenCredentials()
+        {
+            // var result = new EmbedConfig { Username = username, Roles = roles };
+            var error = GetWebConfigErrors();
+            if (error != null)
+            {
+                m_embedConfig.ErrorMessage = error;
+                return false;
+            }
+
+            // Authenticate using created credentials
+            AuthenticationResult authenticationResult = null;
+            try
+            {
+                authenticationResult = await DoAuthentication();
+            }
+            catch (AggregateException exc)
+            {
+                m_embedConfig.ErrorMessage = exc.InnerException.Message;
+                return false;
+            }
+
+            if (authenticationResult == null)
+            {
+                m_embedConfig.ErrorMessage = "Authentication Failed.";
+                return false;
+            }
+
+            m_tokenCredentials = new TokenCredentials(authenticationResult.AccessToken, "Bearer");
+            return true;
+        } //GetTokenCredentials
+
+        private string GetWebConfigErrors()
+        {
+            // Application Id must have a value.
+            if (string.IsNullOrWhiteSpace(ApplicationId))
+            {
+                return "ApplicationId is empty. please register your application as Native app in https://dev.powerbi.com/apps and fill client Id in web.config.";
+            }
+
+            // Application Id must be a Guid object.
+            Guid result;
+            if (!Guid.TryParse(ApplicationId, out result))
+            {
+                return "ApplicationId must be a Guid object. please register your application as Native app in https://dev.powerbi.com/apps and fill application Id in web.config.";
+            }
+
+            // Workspace Id must have a value.
+            if (string.IsNullOrWhiteSpace(WorkspaceId))
+            {
+                return "WorkspaceId is empty. Please select a group you own and fill its Id in web.config";
+            }
+
+            // Workspace Id must be a Guid object.
+            if (!Guid.TryParse(WorkspaceId, out result))
+            {
+                return "WorkspaceId must be a Guid object. Please select a workspace you own and fill its Id in web.config";
+            }
+
+            if (AuthenticationType.Equals("MasterUser"))
+            {
+                // Username must have a value.
+                if (string.IsNullOrWhiteSpace(Username))
+                {
+                    return "Username is empty. Please fill Power BI username in web.config";
+                }
+
+                // Password must have a value.
+                if (string.IsNullOrWhiteSpace(Password))
+                {
+                    return "Password is empty. Please fill password of Power BI username in web.config";
+                }
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(ApplicationSecret))
+                {
+                    return "ApplicationSecret is empty. please register your application as Web app and fill appSecret in web.config.";
+                }
+
+                // Must fill tenant Id
+                if (string.IsNullOrWhiteSpace(Tenant))
+                {
+                    return "Invalid Tenant. Please fill Tenant ID in Tenant under web.config";
+                }
+            }
+
+            return null;
+        } //GetWebConfigErrors
+
+        private async Task<AuthenticationResult> DoAuthentication()
+        {
+            AuthenticationResult authenticationResult = null;
+            if (AuthenticationType.Equals("MasterUser"))
+            {
+                var authenticationContext = new AuthenticationContext(AuthorityUrl);
+
+                // Authentication using master user credentials
+                var credential = new UserPasswordCredential(Username, Password);
+                authenticationResult = authenticationContext.AcquireTokenAsync(ResourceUrl, ApplicationId, credential).Result;
+            }
+            else
+            {
+                // For app only authentication, we need the specific tenant id in the authority url
+                var tenantSpecificURL = AuthorityUrl.Replace("common", Tenant);
+                var authenticationContext = new AuthenticationContext(tenantSpecificURL);
+
+                // Authentication using app credentials
+                var credential = new ClientCredential(ApplicationId, ApplicationSecret);
+                authenticationResult = await authenticationContext.AcquireTokenAsync(ResourceUrl, credential);
+            }
+
+            return authenticationResult;
+        } //DoAuthentication
+    }
+}
